83532ce2ec342c15feb54f962ad4d420b4f92cee
luohui <525078676@qq.com>
2017-03-12 11:31:04 +0800

    增加测试包

--- package-manager/src/test/java/BaseSpringTest.java
+++ package-manager/src/test/java/BaseSpringTest.java
@@ -3,9 +3,16 @@
  * Copyright (c) 2011-2016 All Rights Reserved.
  */
 
+import junit.framework.TestCase;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @author luohui
  * @version Id: BaseSpringTest.java, v 0.1 2016/12/2 16:19 luohui Exp $$
  */
-public class BaseSpringTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring/applicationContext.xml")
+public abstract class BaseSpringTest extends TestCase {
 }
